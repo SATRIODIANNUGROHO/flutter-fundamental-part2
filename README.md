6ea82a810deb05768997740b1af66edf36997462
SATRIODIANNUGROHO <satriodiannugroho@gmail.com>
2025-10-18 23:41:33 +0700

    Create a private method named buildButtonColumn() to build a button column containing icons and text. This method accepts color, icon, and text parameters so that it can be used repeatedly with the same appearance.

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Creating the initial structure for the layout
 Adding column widgets, text (while adjusting padding and color), and icons (while adjusting size and color)
 
 ![screenshot hello_world](images/2.jpg)
+
+Create a private method named buildButtonColumn() to build a button column containing icons and text. This method accepts color, icon, and text parameters so that it can be used repeatedly with the same appearance.
+
+![screenshot hello_world](images/3.jpg)
--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
                 Text(
                   'Batu, Malang, Indonesia',
                   style: TextStyle(
-                    color: Colors.grey,
+                    color: Colors.grey[500],
                   ),
                 ),
               ],
@@ -40,6 +40,17 @@ class MyApp extends StatelessWidget {
           const Text('41'),
         ],
       ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
     );
 
     return MaterialApp(
@@ -57,9 +68,31 @@ class MyApp extends StatelessWidget {
               fit: BoxFit.cover,
             ),
             titleSection,
+            buttonSection,
           ],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
