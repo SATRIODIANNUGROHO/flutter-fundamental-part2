a75d73d39d5bfe5f8294e1ab2b97c1c033e7f58a
SATRIODIANNUGROHO <satriodiannugroho@gmail.com>
2025-10-18 22:45:27 +0700

    Adding column widgets, text (while adjusting padding and color), and icons (while adjusting size and color)

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Creating the initial structure for the layout
 
 ![screenshot hello_world](images/1.jpg)
+
+Adding column widgets, text (while adjusting padding and color), and icons (while adjusting size and color)
+
+![screenshot hello_world](images/2.jpg)
--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,57 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Nama dan NIM Anda',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Satrio Dian Nugroho (2341760113)'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: ListView(
+          children: [
+            Image.asset(
+              'assets/images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+          ],
         ),
       ),
     );
